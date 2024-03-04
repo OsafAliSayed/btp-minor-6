3ae1b01b27940eb6ea7b94f2d88c46fab07ab3c5
osafalisayed <aousafalisayedb2@gmail.com>
2024-03-04 10:42:38 +0530

    Preprocessing upload

--- alexnet.py
+++ alexnet.py
@@ -1,114 +1,47 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import tensorflow as tf
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from tensorflow import keras
-import matplotlib.pyplot as plt
-import os
-import time
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+# Load the CSV data
+data = pd.read_csv('./data/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv')
 
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
- 
-# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+# Assume that the last column is the target variable and the rest are features
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
-(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+# Encode the target variable if it's categorical
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
 
-CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-train_ds=tf.data.Dataset.from_tensor_slices((train_images,train_labels))
-test_ds=tf.data.Dataset.from_tensor_slices((test_images,test_labels))
-
-plt.figure(figsize=(30,30))
-for i,(image,label) in enumerate(train_ds.take(20)):
-    #print(label)
-    ax=plt.subplot(5,5,i+1)
-    plt.imshow(image)
-    plt.title(CLASS_NAMES[label.numpy()[0]])
-    plt.axis('off')
-    
-def process_image(image,label):
-    image=tf.image.per_image_standardization(image)
-    image=tf.image.resize(image,(64,64))
-    
-    return image,label
-
-train_ds_size=tf.data.experimental.cardinality(train_ds).numpy()
-test_ds_size=tf.data.experimental.cardinality(test_ds).numpy()
-print('Train size:',train_ds_size)
-print('Test size:',test_ds_size)
-
-train_ds=(train_ds
-          .map(process_image)
-          .shuffle(buffer_size=train_ds_size)
-          .batch(batch_size=32,drop_remainder=True)
-         )
-test_ds=(test_ds
-          .map(process_image)
-          .shuffle(buffer_size=test_ds_size)
-          .batch(batch_size=32,drop_remainder=True)
-         )
-
-model=keras.models.Sequential([
-    keras.layers.Conv2D(filters=128, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(64,64,3)),
+# Define the AlexNet model
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
     keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(2,2)),
+    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
     keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
     keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(3,3)),
-    keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=384, kernel_size=(1,1), strides=(1,1), activation='relu', padding="same"),
     keras.layers.BatchNormalization(),
     keras.layers.Conv2D(filters=256, kernel_size=(1,1), strides=(1,1), activation='relu', padding="same"),
     keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(filters=256, kernel_size=(1,1), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(2,2)),
+    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
     keras.layers.Flatten(),
-    keras.layers.Dense(1024,activation='relu'),
+    keras.layers.Dense(4096, activation='relu'),
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(1024,activation='relu'),
+    keras.layers.Dense(4096, activation='relu'),
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(10,activation='softmax')  
-    
-    
+    keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.compile(
-    loss='sparse_categorical_crossentropy',
-    optimizer=tf.optimizers.SGD(lr=0.001),
-    metrics=['accuracy']    
-)
-model.summary()
-
-history=model.fit(
-    train_ds,
-    epochs=50,
-    validation_data=test_ds,
-    validation_freq=1
-)
-
-model.history.history.keys()
-
-f,ax=plt.subplots(2,1,figsize=(10,10)) 
-
-#Assigning the first subplot to graph training loss and validation loss
-ax[0].plot(model.history.history['loss'],color='b',label='Training Loss')
-ax[0].plot(model.history.history['val_loss'],color='r',label='Validation Loss')
-
-#Plotting the training accuracy and validation accuracy
-ax[1].plot(model.history.history['accuracy'],color='b',label='Training  Accuracy')
-ax[1].plot(model.history.history['val_accuracy'],color='r',label='Validation Accuracy')
-
-plt.legend()
-
-print('Accuracy Score = ',np.max(history.history['val_accuracy']))
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
